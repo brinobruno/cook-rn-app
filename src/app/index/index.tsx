4c3fdf23ff3543d65fdf5983a2b8a39250586f5c
Bruno Correa <brunosantos6ft@gmail.com>
2024-03-05 10:43:40 -0300

    chore: Base project structure

---
+++ src/app/index/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Escolha os produtos</Text>
+      <Text style={styles.title}>
+        Descubra receitas baseadas nos produtos que você escolheu
+      </Text>
+    </View>
+  )
+}
